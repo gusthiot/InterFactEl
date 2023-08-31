695e38398384ad6d939609b32bde8d8a16db8bbe
Christophe Gusthiot <i2g@gusthiot.ch>
2023-08-31 09:24:30 +0200

    tequila updated

--- session.php
+++ session.php
@@ -3,10 +3,10 @@
 require_once("src/Superviseur.php");
 require_once("src/Gestionnaire.php");
 require_once("src/Message.php");
-require_once("commons/tequila.php");
+require_once("tequila-php-client/tequila.php");
 require_once("config.php");
 
-$oClient = new TequilaClient();
+$oClient = new TequilaClient('https://tequila.epfl.ch', 86400, "InterFactEl");
 $oClient->Authenticate();
 $login = $oClient->getValue('user');
 
@@ -14,3 +14,4 @@ $superviseur = new Superviseur();
 $gestionnaire = new Gestionnaire();
 $messages = new Message();
 //$login = "gusthiot";
+//$_SESSION['user'] = $login;
