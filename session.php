50bcbf6b5055874ee093b2c7dcf406f95c97dd94
Christophe Gusthiot <i2g@gusthiot.ch>
2023-11-11 12:17:46 +0100

    own tequila

--- session.php
+++ session.php
@@ -3,13 +3,11 @@
 require_once("src/Superviseur.php");
 require_once("src/Gestionnaire.php");
 require_once("src/Message.php");
-require_once("tequila-php-client/tequila.php");
-//require_once("commons/tequila.php");
+require_once("commons/Tequila.php");
 require_once("config.php");
 
 session_start();
 $oClient = new TequilaClient('https://tequila.epfl.ch', 86400, "InterFactEl", "", TequilaClient::LANGUAGE_FRENCH);
-//$oClient->Authenticate();
 $oClient->authenticate(['uniqueid'], "", 'group=cmi-fact');
 //$oClient->logout();
 
