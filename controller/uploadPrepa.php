fd02b2fa38c1b40f24f15088bd24410b532ad0d6
Christophe Gusthiot <i2g@gusthiot.ch>
2024-06-03 10:20:09 +0200

    problème

--- controller/uploadPrepa.php
+++ controller/uploadPrepa.php
@@ -140,7 +140,7 @@ function runPrefa($tmpDir, $path, $params, $sciper, $plateforme, $unique, $messa
     $month = $params->getParam('Month');
     $year = $params->getParam('Year');
     $type = $params->getParam('Type');
-    $cmd = '/usr/bin/python3.10 ../PyFactEl/main.py -e '.$tmpDir.' -g -d '.DATA.' -u'.$unique.' -s '.$sciper.' -l '.$_SESSION['user'];
+    $cmd = '/usr/bin/python3.10 ../PyFactEl/main.py -e '.$tmpDir.' -g -n -d '.DATA.' -u'.$unique.' -s '.$sciper.' -l '.$_SESSION['user'];
     $result = shell_exec($cmd);
     $mstr = State::addToMonth($month, 0);
     if(substr($result, 0, 2) === "OK") {
