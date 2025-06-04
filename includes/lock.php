e83833bef483a9071cec3473be984c1e6ebf6841
Christophe Gusthiot <i2g@gusthiot.ch>
2025-06-04 17:42:16 +0200

    gaspar & lock error

--- includes/lock.php
+++ includes/lock.php
@@ -3,19 +3,20 @@
 /**
  * checks if a process is running, and if it's the case, displays a message, and disables access to processing
  */
+$disabled = "";
+$dlTxt = "";
+$lockedPlate = "";
+$lockedRun = "";
+$lockedProcessus = "";
+$lockUser = "";
+$lockProcess = "";
 if(DATA_GEST) {
-    $disabled = "";
-    $dlTxt = "";
     $lockUser = Lock::loadByName(USER.".lock");
     if(!empty($lockUser)) {
         $disabled = "disabled";
         $dlTxt = '<a href="#" id="download-generated">Vous avez un dossier à télécharger avant de pouvoir faire d’autres actions.</a>';
     }
-
     $lockProcess = Lock::load("./", "process");
-    $lockedPlate = "";
-    $lockedRun = "";
-    $lockedProcessus = "";
     if(!empty($lockProcess)) {
         $disabled = "disabled";
         $lockedTab = explode(" ", $lockProcess);
