9d2f852330e95fdeb07b7569646950960b5c3e88
Christophe Gusthiot <i2g@gusthiot.ch>
2025-06-10 13:03:42 +0200

    effacer entra token au logout

--- logout.php
+++ logout.php
@@ -2,9 +2,11 @@
 
 require_once("session.inc");
 
-if(!DEV_MODE) {
-    $_SESSION = array();
-    session_destroy();
+if(DEV_MODE) {
+    header('Location: index.php');
 }
-header('Location: index.php');
+else {
+    $_SESSION['logout'] = true;
+    header('Location: https://cmifact.epfl.ch/entra/entra.php');
+}
 
