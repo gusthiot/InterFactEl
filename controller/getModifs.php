e7ccd591cb1d790f046ed1ad839d84a1c429679a
Christophe Gusthiot <i2g@gusthiot.ch>
2025-06-23 16:41:29 +0200

    dataGest in getModifs

--- controller/getModifs.php
+++ controller/getModifs.php
@@ -9,7 +9,7 @@ require_once("../session.inc");
 if(isset($_POST["plate"]) && isset($_POST["year"]) && isset($_POST["month"]) && isset($_POST["version"]) && isset($_POST["run"])) {
     checkPlateforme("facturation", $_POST["plate"]);
     $dir = DATA.$_POST['plate']."/".$_POST['year']."/".$_POST['month']."/".$_POST['version']."/".$_POST['run'];
-    $name = $dataGest['facturation'][$_POST['plate']];
+    $name = DATA_GEST['facturation'][$_POST['plate']];
     $suf = "_".$name."_".$_POST['year']."_".$_POST['month']."_".$_POST['version'];
     $html = "";
     $html .= table(Modif::load($dir."/Modif-factures".$suf.".csv"), "get-modif", "Factures-modifs", "modifs", [7, 8]);
