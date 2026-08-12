d746566ecc04469c1f6bd47a419ccd7324ac38ac
Christophe Gusthiot <i2g@gusthiot.ch>
2026-08-12 15:22:49 +0200

    concat tarifs

--- js/tarifs/espace.js
+++ js/tarifs/espace.js
@@ -5,6 +5,36 @@ const plateforme = $('#container').data('plateforme');
 const m0 = $('#tarifs-space').data('m0');
 const m0Status = $('#tarifs-space').data('status');
 
+/** LISTE */
+
+$("#list-tab").on("click", function() {
+    window.location.href = "tarifs.php?plateforme="+plateforme;
+});
+
+$(document).on("click", ".all", function() {
+    const tab = $(this).attr('id').split("-");
+    const run = $(this).data("run");
+    const version = $(this).data("version");
+    window.location.href = "controller/download.php?type=alltarifs&plate="+plateforme+"&year="+tab[1]+"&month="+tab[2]+"&version="+version+"&run="+run;
+});
+
+$(document).on("click", ".etiquette", function() {
+    const tab = $(this).attr('id').split("-");
+    $.post("controller/getLabel.php", {plate: plateforme, right: "tarifs", year: tab[1], month: tab[2]}, function (data) {
+        $('#label-'+tab[1]+'-'+tab[2]).html(data);
+    });
+});
+
+$(document).on("click", "#save-label", function() {
+    const tab = $(this).parent().parent().attr('id').split("-");
+    const txt = $('#label-area').val();
+    $.post("controller/saveLabel.php", {txt: txt, right: "tarifs", plate: plateforme, year: tab[1], month: tab[2]}, function () {
+        window.location.href = "tarifs.php?plateforme="+plateforme;
+    });
+});
+
+/** ESPACE */
+
 /** Left */
 
 $("#tarifs-read").on("click", function() {
